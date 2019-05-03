616810bb4fcb04bba2c8cdc9dfaf76160bc78d43
Lexie <sb.alexandria@gmail.com>
2019-05-03 22:38:08 +0700

    Added new SVG, tested some SVG animation

---
+++ src/components/ArtWork.vue
@@ -0,0 +1,309 @@
+<template>
+  <div class="artwork">
+    
+<svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" viewBox="0 0 800.3 802.7" x="0" y="0" version="1.1" xml:space="preserve">
+  <g id="artwork">
+    <path id="background" opacity="0.5"/>
+    <g id="white-lined-circle-bottom" fill="#FFF">
+      <path d="M455.7 461.6l26.2-26.2c-5.3 3.1-10.3 6.8-14.9 11.4-4.5 4.5-8.3 9.5-11.3 14.8zM498.8 428.4c-2.7.7-5.3 1.6-7.8 2.6l-39.7 39.7c-1 2.6-1.9 5.2-2.6 7.8l50.1-50.1zM510.9 426.3c-2 .2-4.1.5-6.1.8l-57.6 57.6c-.4 2-.6 4-.8 6.1l64.5-64.5zM446.3 500.8l74.7-74.7c-1.7-.1-3.4-.1-5.2-.1l-69.6 69.6c0 1.8 0 3.5.1 5.2zM530 427.1c-1.5-.3-3-.5-4.6-.7l-78.7 78.7c.2 1.5.4 3.1.7 4.6l82.6-82.6zM538 429.1c-1.4-.4-2.7-.8-4.1-1.1l-85.7 85.7c.3 1.4.7 2.8 1.1 4.1l88.7-88.7zM545.3 431.7c-1.2-.5-2.5-1-3.8-1.5l-91 91c.5 1.3 1 2.5 1.5 3.8l93.3-93.3zM552 435c-1.1-.6-2.3-1.2-3.4-1.8l-95 95c.6 1.2 1.2 2.3 1.8 3.4L552 435zM558.1 438.9c-1-.7-2.1-1.4-3.2-2.1L457 534.7c.7 1.1 1.4 2.1 2.1 3.2l99-99zM563.7 443.3c-.9-.8-1.9-1.6-2.9-2.4l-99.6 99.6c.8 1 1.5 1.9 2.4 2.9l100.1-100.1zM468.4 548.5l100.4-100.4c-.4-.4-.8-.9-1.3-1.3s-.9-.9-1.3-1.3L465.7 545.9c.4.4.8.9 1.3 1.3.5.5.9.9 1.4 1.3zM573.4 453.5c-.8-1-1.5-1.9-2.4-2.9L470.8 550.7c.9.8 1.9 1.6 2.9 2.4l99.7-99.6zM577.5 459.3c-.7-1.1-1.4-2.1-2.1-3.2L476.5 555c1 .7 2.1 1.4 3.2 2.1l97.8-97.8zM581 465.7c-.6-1.2-1.2-2.3-1.8-3.4L482.5 559c1.1.6 2.3 1.2 3.4 1.8l95.1-95.1zM584 472.7c-.5-1.3-1-2.5-1.5-3.8l-93.3 93.3c1.2.5 2.5 1 3.8 1.5l91-91zM586.3 480.4c-.3-1.4-.7-2.8-1.1-4.1L496.5 565c1.4.4 2.7.8 4.1 1.1l85.7-85.7zM587.8 488.8c-.2-1.5-.4-3.1-.7-4.6l-82.6 82.6c1.5.3 3 .5 4.6.7l78.7-78.7zM518.6 568l69.6-69.6c0-1.7 0-3.5-.1-5.2l-74.7 74.7c1.7.1 3.5.1 5.2.1zM587.2 509.4c.4-2 .6-4 .8-6.1l-64.4 64.4c2-.2 4.1-.5 6.1-.8l57.5-57.5zM583.3 523.3c1-2.6 1.9-5.2 2.6-7.8l-50.2 50.2c2.7-.7 5.3-1.6 7.8-2.6l39.8-39.8zM578.8 532.4l-26.2 26.2c5.3-3.1 10.3-6.8 14.9-11.4 4.5-4.5 8.3-9.5 11.3-14.8z"/>
+    </g>
+    <defs>
+      <filter id="Adobe_OpacityMaskFilter" width="444.5" height="444.5" x="177.7" y="77.7" filterUnits="userSpaceOnUse">
+        <feFlood flood-color="#fff" flood-opacity="1" result="back"/>
+        <feBlend in="SourceGraphic" in2="back" mode="normal"/>
+      </filter>
+    </defs>
+    <mask id="shadow-middle-circle_1_" width="444.5" height="444.5" x="177.7" y="77.7" maskUnits="userSpaceOnUse">
+      <g filter="url(#Adobe_OpacityMaskFilter)">
+        <defs>
+          <filter id="Adobe_OpacityMaskFilter_1_" width="444.5" height="444.5" x="177.7" y="77.7" filterUnits="userSpaceOnUse">
+            <feFlood flood-color="#fff" flood-opacity="1" result="back"/>
+            <feBlend in="SourceGraphic" in2="back" mode="normal"/>
+          </filter>
+        </defs>
+        <radialGradient id="SVGID_1_" cx="400" cy="300" r="222.2578" gradientUnits="userSpaceOnUse">
+          <stop offset=".6029" stop-color="#fff"/>
+          <stop offset=".7509" stop-color="#fdfdfd"/>
+          <stop offset=".8042" stop-color="#f6f6f6"/>
+          <stop offset=".8422" stop-color="#ebebeb"/>
+          <stop offset=".8729" stop-color="#dadada"/>
+          <stop offset=".8992" stop-color="#c4c4c4"/>
+          <stop offset=".9226" stop-color="#a8a8a8"/>
+          <stop offset=".9437" stop-color="#888"/>
+          <stop offset=".963" stop-color="#626262"/>
+          <stop offset=".9811" stop-color="#373737"/>
+          <stop offset=".9971" stop-color="#090909"/>
+          <stop offset="1" stop-color="#000"/>
+        </radialGradient>
+        <circle cx="400" cy="300" r="222.3" fill="url(#SVGID_1_)" mask="url(#shadow-middle-circle_1_)"/>
+      </g>
+    </mask>
+    <radialGradient id="shadow-middle-circle_2_" cx="400" cy="300" r="222.2578" gradientUnits="userSpaceOnUse">
+      <stop offset=".6029" stop-color="#2d002a"/>
+      <stop offset=".7881" stop-color="#2e002b"/>
+      <stop offset=".8548" stop-color="#31002e"/>
+      <stop offset=".9023" stop-color="#360033"/>
+      <stop offset=".9408" stop-color="#3c0439"/>
+      <stop offset=".9733" stop-color="#450d42"/>
+      <stop offset="1" stop-color="#4f164b"/>
+    </radialGradient>
+    <circle id="shadow-middle-circle" cx="400" cy="300" r="222.3" fill="url(#shadow-middle-circle_2_)" opacity=".75"/>
+    <!-- <g id="top-dotted-square" fill="#FFF">
+      <circle cx="370.8" cy="32.5" r="4.2"/>
+      <circle cx="387.5" cy="32.5" r="4.2"/>
+      <circle cx="404.2" cy="32.5" r="4.2"/>
+      <circle cx="420.8" cy="32.5" r="4.2"/>
+      <circle cx="437.5" cy="32.5" r="4.2"/>
+      <circle cx="454.2" cy="32.5" r="4.2"/>
+      <circle cx="470.8" cy="32.5" r="4.2"/>
+      <circle cx="487.5" cy="32.5" r="4.2"/>
+      <circle cx="370.8" cy="49.2" r="4.2"/>
+      <circle cx="387.5" cy="49.2" r="4.2"/>
+      <circle cx="404.2" cy="49.2" r="4.2"/>
+      <circle cx="420.8" cy="49.2" r="4.2"/>
+      <circle cx="437.5" cy="49.2" r="4.2"/>
+      <circle cx="454.2" cy="49.2" r="4.2"/>
+      <circle cx="470.8" cy="49.2" r="4.2"/>
+      <circle cx="487.5" cy="49.2" r="4.2"/>
+      <circle cx="370.8" cy="65.9" r="4.2"/>
+      <circle cx="387.5" cy="65.9" r="4.2"/>
+      <circle cx="404.2" cy="65.9" r="4.2"/>
+      <circle cx="420.8" cy="65.9" r="4.2"/>
+      <circle cx="437.5" cy="65.9" r="4.2"/>
+      <circle cx="454.2" cy="65.9" r="4.2"/>
+      <circle cx="470.8" cy="65.9" r="4.2"/>
+      <circle cx="487.5" cy="65.9" r="4.2"/>
+      <circle cx="370.8" cy="82.5" r="4.2"/>
+      <circle cx="387.5" cy="82.5" r="4.2"/>
+      <circle cx="404.2" cy="82.5" r="4.2"/>
+      <circle cx="420.8" cy="82.5" r="4.2"/>
+      <circle cx="437.5" cy="82.5" r="4.2"/>
+      <circle cx="454.2" cy="82.5" r="4.2"/>
+      <circle cx="470.8" cy="82.5" r="4.2"/>
+      <circle cx="487.5" cy="82.5" r="4.2"/>
+      <circle cx="370.8" cy="99.2" r="4.2"/>
+      <circle cx="387.5" cy="99.2" r="4.2"/>
+      <circle cx="404.2" cy="99.2" r="4.2"/>
+      <circle cx="420.8" cy="99.2" r="4.2"/>
+      <circle cx="437.5" cy="99.2" r="4.2"/>
+      <circle cx="454.2" cy="99.2" r="4.2"/>
+      <circle cx="470.8" cy="99.2" r="4.2"/>
+      <circle cx="487.5" cy="99.2" r="4.2"/>
+      <circle cx="370.8" cy="115.9" r="4.2"/>
+      <circle cx="387.5" cy="115.9" r="4.2"/>
+      <circle cx="404.2" cy="115.9" r="4.2"/>
+      <circle cx="420.8" cy="115.9" r="4.2"/>
+      <circle cx="437.5" cy="115.9" r="4.2"/>
+      <circle cx="454.2" cy="115.9" r="4.2"/>
+      <circle cx="470.8" cy="115.9" r="4.2"/>
+      <circle cx="487.5" cy="115.9" r="4.2"/>
+      <circle cx="370.8" cy="132.5" r="4.2"/>
+      <circle cx="387.5" cy="132.5" r="4.2"/>
+      <circle cx="404.2" cy="132.5" r="4.2"/>
+      <circle cx="420.8" cy="132.5" r="4.2"/>
+      <circle cx="437.5" cy="132.5" r="4.2"/>
+      <circle cx="454.2" cy="132.5" r="4.2"/>
+      <circle cx="470.8" cy="132.5" r="4.2"/>
+      <circle cx="487.5" cy="132.5" r="4.2"/>
+      <circle cx="370.8" cy="149.2" r="4.2"/>
+      <circle cx="387.5" cy="149.2" r="4.2"/>
+      <circle cx="404.2" cy="149.2" r="4.2"/>
+      <circle cx="420.8" cy="149.2" r="4.2"/>
+      <circle cx="437.5" cy="149.2" r="4.2"/>
+      <circle cx="454.2" cy="149.2" r="4.2"/>
+      <circle cx="470.8" cy="149.2" r="4.2"/>
+      <circle cx="487.5" cy="149.2" r="4.2"/>
+    </g> -->
+    <circle id="big-middle-circle-2" cx="400" cy="300" r="197" fill="#EE2356"/>
+    <circle id="big-middle-circle-1" cx="400" cy="300" r="197" fill="#EE2356"/>
+    <path id="big-circle-pink-border2" fill="none" stroke="#774A5F" stroke-miterlimit="10" stroke-width="4" d="M582.5 482.5c100.8-100.8 100.8-264.1 0-364.9"/>
+    <path id="big-circle-pink-border1" fill="none" stroke="#EE2356" stroke-miterlimit="10" stroke-width="4" d="M622.3 300c0-122.7-99.5-222.3-222.3-222.3"/>
+    <!-- <path id="yellow-triangle-top" fill="#D7DF23" d="M544.3 0l-27.1 46.9L490.1 0z"/> -->
+    <path id="pink-triangle-left" fill="#EE2356" d="M0 34.9l63.2 36.5L0 107.9z"/>
+    <!-- <g id="right-dotted-square-transparent" fill="#774A5F">
+      <circle cx="679.2" cy="215" r="4.2"/>
+      <circle cx="695.8" cy="215" r="4.2"/>
+      <circle cx="712.5" cy="215" r="4.2"/>
+      <circle cx="729.2" cy="215" r="4.2"/>
+      <circle cx="745.8" cy="215" r="4.2"/>
+      <circle cx="762.5" cy="215" r="4.2"/>
+      <circle cx="779.2" cy="215" r="4.2"/>
+      <circle cx="795.8" cy="215" r="4.2"/>
+      <circle cx="679.2" cy="231.7" r="4.2"/>
+      <circle cx="695.8" cy="231.7" r="4.2"/>
+      <circle cx="712.5" cy="231.7" r="4.2"/>
+      <circle cx="729.2" cy="231.7" r="4.2"/>
+      <circle cx="745.8" cy="231.7" r="4.2"/>
+      <circle cx="762.5" cy="231.7" r="4.2"/>
+      <circle cx="779.2" cy="231.7" r="4.2"/>
+      <circle cx="795.8" cy="231.7" r="4.2"/>
+      <circle cx="679.2" cy="248.4" r="4.2"/>
+      <circle cx="695.8" cy="248.4" r="4.2"/>
+      <circle cx="712.5" cy="248.4" r="4.2"/>
+      <circle cx="729.2" cy="248.4" r="4.2"/>
+      <circle cx="745.8" cy="248.4" r="4.2"/>
+      <circle cx="762.5" cy="248.4" r="4.2"/>
+      <circle cx="779.2" cy="248.4" r="4.2"/>
+      <circle cx="795.8" cy="248.4" r="4.2"/>
+      <circle cx="679.2" cy="265" r="4.2"/>
+      <circle cx="695.8" cy="265" r="4.2"/>
+      <circle cx="712.5" cy="265" r="4.2"/>
+      <circle cx="729.2" cy="265" r="4.2"/>
+      <circle cx="745.8" cy="265" r="4.2"/>
+      <circle cx="762.5" cy="265" r="4.2"/>
+      <circle cx="779.2" cy="265" r="4.2"/>
+      <circle cx="795.8" cy="265" r="4.2"/>
+      <circle cx="679.2" cy="281.7" r="4.2"/>
+      <circle cx="695.8" cy="281.7" r="4.2"/>
+      <circle cx="712.5" cy="281.7" r="4.2"/>
+      <circle cx="729.2" cy="281.7" r="4.2"/>
+      <circle cx="745.8" cy="281.7" r="4.2"/>
+      <circle cx="762.5" cy="281.7" r="4.2"/>
+      <circle cx="779.2" cy="281.7" r="4.2"/>
+      <circle cx="795.8" cy="281.7" r="4.2"/>
+      <circle cx="679.2" cy="298.4" r="4.2"/>
+      <circle cx="695.8" cy="298.4" r="4.2"/>
+      <circle cx="712.5" cy="298.4" r="4.2"/>
+      <circle cx="729.2" cy="298.4" r="4.2"/>
+      <circle cx="745.8" cy="298.4" r="4.2"/>
+      <circle cx="762.5" cy="298.4" r="4.2"/>
+      <circle cx="779.2" cy="298.4" r="4.2"/>
+      <circle cx="795.8" cy="298.4" r="4.2"/>
+      <circle cx="679.2" cy="315" r="4.2"/>
+      <circle cx="695.8" cy="315" r="4.2"/>
+      <circle cx="712.5" cy="315" r="4.2"/>
+      <circle cx="729.2" cy="315" r="4.2"/>
+      <circle cx="745.8" cy="315" r="4.2"/>
+      <circle cx="762.5" cy="315" r="4.2"/>
+      <circle cx="779.2" cy="315" r="4.2"/>
+      <circle cx="795.8" cy="315" r="4.2"/>
+      <circle cx="679.2" cy="331.7" r="4.2"/>
+      <circle cx="695.8" cy="331.7" r="4.2"/>
+      <circle cx="712.5" cy="331.7" r="4.2"/>
+      <circle cx="729.2" cy="331.7" r="4.2"/>
+      <circle cx="745.8" cy="331.7" r="4.2"/>
+      <circle cx="762.5" cy="331.7" r="4.2"/>
+      <circle cx="779.2" cy="331.7" r="4.2"/>
+      <circle cx="795.8" cy="331.7" r="4.2"/>
+    </g> -->
+    <!-- <g id="left-dotted-square-bottom" fill="#FFF">
+      <circle cx="4.2" cy="479.2" r="4.2"/>
+      <circle cx="20.8" cy="479.2" r="4.2"/>
+      <circle cx="37.5" cy="479.2" r="4.2"/>
+      <circle cx="54.2" cy="479.2" r="4.2"/>
+      <circle cx="70.8" cy="479.2" r="4.2"/>
+      <circle cx="87.5" cy="479.2" r="4.2"/>
+      <circle cx="104.2" cy="479.2" r="4.2"/>
+      <circle cx="120.8" cy="479.2" r="4.2"/>
+      <circle cx="4.2" cy="495.8" r="4.2"/>
+      <circle cx="20.8" cy="495.8" r="4.2"/>
+      <circle cx="37.5" cy="495.8" r="4.2"/>
+      <circle cx="54.2" cy="495.8" r="4.2"/>
+      <circle cx="70.8" cy="495.8" r="4.2"/>
+      <circle cx="87.5" cy="495.8" r="4.2"/>
+      <circle cx="104.2" cy="495.8" r="4.2"/>
+      <circle cx="120.8" cy="495.8" r="4.2"/>
+      <circle cx="4.2" cy="512.5" r="4.2"/>
+      <circle cx="20.8" cy="512.5" r="4.2"/>
+      <circle cx="37.5" cy="512.5" r="4.2"/>
+      <circle cx="54.2" cy="512.5" r="4.2"/>
+      <circle cx="70.8" cy="512.5" r="4.2"/>
+      <circle cx="87.5" cy="512.5" r="4.2"/>
+      <circle cx="104.2" cy="512.5" r="4.2"/>
+      <circle cx="120.8" cy="512.5" r="4.2"/>
+      <circle cx="4.2" cy="529.2" r="4.2"/>
+      <circle cx="20.8" cy="529.2" r="4.2"/>
+      <circle cx="37.5" cy="529.2" r="4.2"/>
+      <circle cx="54.2" cy="529.2" r="4.2"/>
+      <circle cx="70.8" cy="529.2" r="4.2"/>
+      <circle cx="87.5" cy="529.2" r="4.2"/>
+      <circle cx="104.2" cy="529.2" r="4.2"/>
+      <circle cx="120.8" cy="529.2" r="4.2"/>
+      <circle cx="4.2" cy="545.8" r="4.2"/>
+      <circle cx="20.8" cy="545.8" r="4.2"/>
+      <circle cx="37.5" cy="545.8" r="4.2"/>
+      <circle cx="54.2" cy="545.8" r="4.2"/>
+      <circle cx="70.8" cy="545.8" r="4.2"/>
+      <circle cx="87.5" cy="545.8" r="4.2"/>
+      <circle cx="104.2" cy="545.8" r="4.2"/>
+      <circle cx="120.8" cy="545.8" r="4.2"/>
+      <circle cx="4.2" cy="562.5" r="4.2"/>
+      <circle cx="20.8" cy="562.5" r="4.2"/>
+      <circle cx="37.5" cy="562.5" r="4.2"/>
+      <circle cx="54.2" cy="562.5" r="4.2"/>
+      <circle cx="70.8" cy="562.5" r="4.2"/>
+      <circle cx="87.5" cy="562.5" r="4.2"/>
+      <circle cx="104.2" cy="562.5" r="4.2"/>
+      <circle cx="120.8" cy="562.5" r="4.2"/>
+      <circle cx="4.2" cy="579.2" r="4.2"/>
+      <circle cx="20.8" cy="579.2" r="4.2"/>
+      <circle cx="37.5" cy="579.2" r="4.2"/>
+      <circle cx="54.2" cy="579.2" r="4.2"/>
+      <circle cx="70.8" cy="579.2" r="4.2"/>
+      <circle cx="87.5" cy="579.2" r="4.2"/>
+      <circle cx="104.2" cy="579.2" r="4.2"/>
+      <circle cx="120.8" cy="579.2" r="4.2"/>
+      <circle cx="4.2" cy="595.8" r="4.2"/>
+      <circle cx="20.8" cy="595.8" r="4.2"/>
+      <circle cx="37.5" cy="595.8" r="4.2"/>
+      <circle cx="54.2" cy="595.8" r="4.2"/>
+      <circle cx="70.8" cy="595.8" r="4.2"/>
+      <circle cx="87.5" cy="595.8" r="4.2"/>
+      <circle cx="104.2" cy="595.8" r="4.2"/>
+      <circle cx="120.8" cy="595.8" r="4.2"/>
+    </g> -->
+    <g id="left-yellow-circle-lines" fill="#D7DF23">
+      <path d="M126.7 177.6l32.5-32.5c-6.6 3.8-12.8 8.5-18.4 14.1-5.6 5.6-10.3 11.8-14.1 18.4zM180.2 136.5c-3.3.9-6.5 1.9-9.7 3.2l-49.2 49.2c-1.3 3.2-2.3 6.4-3.2 9.7l62.1-62.1zM195.2 133.8c-2.5.2-5 .6-7.5 1L116.5 206c-.4 2.5-.8 5-1 7.5l79.7-79.7zM115.2 226.1l92.5-92.5c-2.1-.1-4.3-.1-6.4-.1l-86.2 86.2c0 2.2 0 4.3.1 6.4zM218.8 134.9c-1.9-.3-3.8-.6-5.7-.8l-97.4 97.4c.2 1.9.5 3.8.8 5.7l102.3-102.3zM228.7 137.3c-1.7-.5-3.4-1-5.1-1.4L117.5 242c.4 1.7.9 3.4 1.4 5.1l109.8-109.8zM237.7 140.6c-1.5-.7-3.1-1.3-4.7-1.8L120.4 251.5c.6 1.6 1.2 3.1 1.8 4.7l115.5-115.6zM246 144.7c-1.4-.8-2.8-1.5-4.3-2.2L124.1 260.1c.7 1.4 1.5 2.9 2.2 4.3L246 144.7zM253.5 149.5c-1.3-.9-2.6-1.8-3.9-2.6L128.5 268c.8 1.3 1.7 2.6 2.6 3.9l122.4-122.4zM260.5 154.9c-1.2-1-2.4-2-3.6-2.9L133.5 275.3c.9 1.2 1.9 2.4 2.9 3.6l124.1-124zM142.5 285.2l124.3-124.3c-.5-.6-1-1.1-1.6-1.7-.5-.5-1.1-1.1-1.7-1.6L139.2 281.9c.5.6 1 1.1 1.6 1.7s1.1 1 1.7 1.6zM272.5 167.5c-.9-1.2-1.9-2.4-2.9-3.6l-124 124c1.2 1 2.4 2 3.6 2.9l123.3-123.3zM277.5 174.8c-.8-1.3-1.7-2.6-2.6-3.9L152.5 293.3c1.3.9 2.6 1.8 3.9 2.6l121.1-121.1zM281.9 182.7c-.7-1.4-1.5-2.9-2.2-4.3L160 298.1c1.4.8 2.8 1.5 4.3 2.2l117.6-117.6zM285.6 191.3c-.6-1.6-1.2-3.1-1.9-4.7L168.3 302.2c1.5.7 3.1 1.3 4.7 1.8l112.6-112.7zM288.5 200.8c-.4-1.7-.9-3.4-1.4-5.1L177.3 305.5c1.7.5 3.4 1 5.1 1.4l106.1-106.1zM290.3 211.3c-.2-1.9-.5-3.8-.8-5.7L187.2 307.9c1.9.3 3.8.6 5.7.8l97.4-97.4zM204.7 309.3l86.2-86.2c0-2.1 0-4.3-.1-6.4l-92.5 92.5c2.1.1 4.2.1 6.4.1zM289.6 236.7c.4-2.5.8-5 1-7.5L210.8 309c2.5-.2 5-.6 7.5-1l71.3-71.3zM284.7 253.9c1.3-3.2 2.3-6.4 3.2-9.7l-62.1 62.1c3.3-.9 6.5-1.9 9.7-3.2l49.2-49.2zM279.3 265.2l-32.5 32.5c6.6-3.8 12.8-8.5 18.4-14.1 5.6-5.6 10.3-11.8 14.1-18.4z"/>
+    </g>
+    <circle id="left-yellow-circle-bottom" cx="120.4" cy="481.8" r="25.9" fill="#D7DF23"/>
+    <circle id="left-yellow-circle-border-bottom" cx="120.4" cy="481.8" r="46.9" fill="none" stroke="#D7DF23" stroke-miterlimit="10" stroke-width="2"/>
+    <circle id="small-yellow-circle-bottom" cx="588.3" cy="477.6" r="16.9" fill="#D7DF23"/>
+    <circle id="small-white-circle-left" cx="115.7" cy="233.4" r="16.9" fill="#FFF"/>
+    <!-- <path id="top-transparent-small-square" fill="#774A5F" d="M239.5 0h65.9v65.9h-65.9z"/> -->
+    <!-- <path id="right-transparent-small-square" fill="#774A5F" d="M743.6 450.3H800v112.9h-56.4z"/> -->
+    <!-- <path id="left-transparent-small-square" fill="#774A5F" d="M0 265.2h63.2v126.4H0z"/> -->
+    <path id="top-pink-small-square" fill="#EE2356" d="M225.5 18.9h28v28h-28z"/>
+    <path id="right-pink-small-square" fill="#EE2356" d="M727.7 199.6h28v28h-28z"/>
+    <!-- <path id="bottom-small-square" fill="#774A5F" d="M393.5 572h28v28h-28z"/> -->
+    <path id="right-blue-half-circle" fill="#B9E5FB" d="M743.6 335.9c0 31.2 25.3 56.4 56.4 56.4V279.4c-31.2 0-56.4 25.3-56.4 56.5z"/>
+    <path id="left-blue-half-circle" fill="#B9E5FB" d="M25 265.2c0-13.8-11.2-25-25-25v50c13.8 0 25-11.2 25-25z"/>
+    <path id="white-triangle" fill="#FFF" d="M306.1 514.3l-53.7-31 53.7-31z"/>
+    <path id="right-yellow-top-circle" fill="#D7DF23" d="M800 111.5V0H690.7c0 60.8 48.7 110.3 109.3 111.5z"/>
+    <circle id="right-circle-bottom" cx="742.6" cy="563.1" r="12.5" fill="#EE2356"/>
+    <circle id="right-circle-border-bottom" cx="742.6" cy="563.1" r="29.2" fill="none" stroke="#EE2356" stroke-miterlimit="10" stroke-width="2"/>
+  </g>
+</svg>
+  </div>
+</template>
+
+<script>
+import { TimelineLite, TweenMax, Back } from "gsap";
+
+export default {
+  name: "ArtWork",
+  props: {
+    msg: String
+  },
+  mounted() {   
+      // Testing if SVG animation is working 
+         let tl = new TimelineMax();
+			tl.fromTo("#white-lined-circle-bottom", 3, {
+				opacity: 0
+			}, {
+                opacity: 1,                
+				ease: Sine.easeOut
+			}, );
+
+	}
+
+   
+  
+  
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">   
-    <HelloWorld/>
-    
+  <div class="home" id="grid-container">
+    <HelloWorld class="helloworld"/>
+    <!-- <FlowerArea/> -->
+    <ArtWork class="artwork"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import FlowerArea from "@/components/FlowerArea.vue";
+import ArtWork from "@/components/ArtWork.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    FlowerArea,
+    ArtWork
   }
-}
+};
 </script>
+<style scoped lang="scss">
+.home {
+  font-family: "pink", sans-serif;
+  color: #33f;
+  background: #fce4ec;
+}
+
+#grid-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+}
+
+.helloworld {
+  grid-column: 1;
+  grid-row: 1 / 3;
+  justify-self: center;
+}
+
+.artwork {
+  grid-column: 2;
+  grid-row: 1 / 3; 
+  background: rgb(166, 125, 156);
+  padding: 10% 2% 0.5% 2%;
+}
+</style>
