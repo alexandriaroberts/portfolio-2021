1a1d799381e186a345f498e19c940f3976d237a4
Alexandria Roberts <alexandria.roberts@joyjet.com>
2020-04-23 23:48:57 +0800

    projects section

--- src/components/ProjectDetails.vue
+++ src/components/ProjectDetails.vue
@@ -1,110 +1,107 @@
 <template>
   <!-- Project details -->
-  <div class="projects">
-    <h1 class="heading title">Projects</h1>
-    <div class="project__grid">
-      <div
-        v-for="(project, index) in projects"
-        :key="index"
-        ref="projectImage"
-        role="projectItem"
-        class="projectImage"
-      >
-        <!-- <p class="project-title">{{ project.name }}</p> -->
-        <a :href="project.url" target="_blank" rel="noopener">
+
+  <div class="project__grid">
+    <div
+      v-for="(project, index) in projects"
+      :key="index"
+      ref="projectImage"
+      role="projectItem"
+      class="projectImage"
+    >
+      <a :href="project.url" target="_blank" rel="noopener">
         <img
           :src="project.image.imageURL"
           :alt="project.image.alt"
           :title="project.name"
           class="projectImage"
         />
-        </a>
-          <p class="project-title">{{ project.name }}</p>
-      </div>
+      </a>
+      <p class="project-title">{{ project.name }}</p>
     </div>
   </div>
 </template>
 
 <script>
-import main from '../assets/css/main.scss';
-import { TweenMax, Power2, TimelineMax } from 'gsap/TweenMax';
+import main from "../assets/css/main.scss";
+import { TweenMax, Power2, TimelineMax } from "gsap/TweenMax";
 
 export default {
   data() {
     return {
       projects: [
         {
-          name: 'Flower VueJs Game',
-          url: 'https://codepen.io/iamlexieonearth/full/QYNmEa',
+          name: "Flower VueJs Game",
+          url: "https://codepen.io/iamlexieonearth/full/QYNmEa",
           image: {
             imageURL:
-              'https://res.cloudinary.com/dv3lue3qa/image/upload/v1563170890/Flower_Game_in_VueJS.png',
+              "https://res.cloudinary.com/dv3lue3qa/image/upload/v1563170890/Flower_Game_in_VueJS.png",
             alt:
-              'Flower VueJs Game, player clicks on the flower and get points for clickig on them'
+              "Flower VueJs Game, player clicks on the flower and get points for clickig on them"
           }
         },
         {
-          name: 'Space Game with Canvas',
-          url: 'https://codepen.io/iamlexieonearth/full/KEoMye',
+          name: "Space Game with Canvas",
+          url: "https://codepen.io/iamlexieonearth/full/KEoMye",
           image: {
             imageURL:
-              'https://res.cloudinary.com/dv3lue3qa/image/upload/v1563170891/Space_Game_with_Canvas.png',
+              "https://res.cloudinary.com/dv3lue3qa/image/upload/v1563170891/Space_Game_with_Canvas.png",
             alt:
-              'Space Game with Canvas, player plays with key board and catches an alien'
+              "Space Game with Canvas, player plays with key board and catches an alien"
           }
         },
         {
-          name: 'Nasa API VueJS',
-          url: 'https://codepen.io/iamlexieonearth/full/vwPRPY',
+          name: "Nasa API VueJS",
+          url: "https://codepen.io/iamlexieonearth/full/vwPRPY",
           image: {
             imageURL:
-              'https://res.cloudinary.com/dv3lue3qa/image/upload/v1563856744/Nasa-API.png',
-            alt: 'Nasa API VueJS Project to render images from NaSa'
+              "https://res.cloudinary.com/dv3lue3qa/image/upload/v1563856744/Nasa-API.png",
+            alt: "Nasa API VueJS Project to render images from NaSa"
           }
         },
         {
-          name: 'CSS Grid Poster',
-          url: 'https://codepen.io/iamlexieonearth/full/aKOzyL',
+          name: "CSS Grid Poster",
+          url: "https://codepen.io/iamlexieonearth/full/aKOzyL",
           image: {
             imageURL:
-              'https://res.cloudinary.com/dv3lue3qa/image/upload/v1562737397/poster.png',
-            alt: 'CSS Grid Poster'
+              "https://res.cloudinary.com/dv3lue3qa/image/upload/v1562737397/poster.png",
+            alt: "CSS Grid Poster"
           }
         },
         {
-          name: 'Product Landing Page',
-          url: 'https://codepen.io/iamlexieonearth/full/zLzdLJ',
+          name: "Product Landing Page",
+          url: "https://codepen.io/iamlexieonearth/full/zLzdLJ",
           image: {
             imageURL:
-              'https://res.cloudinary.com/dv3lue3qa/image/upload/v1562737777/product-landing-page.png',
-            alt: 'Product Landing Page of Dog Shoes'
+              "https://res.cloudinary.com/dv3lue3qa/image/upload/v1562737777/product-landing-page.png",
+            alt: "Product Landing Page of Dog Shoes"
           }
         },
         {
-          name: 'Survey Form',
-          url: 'https://codepen.io/iamlexieonearth/full/OEqZWJ',
+          name: "Survey Form",
+          url: "https://codepen.io/iamlexieonearth/full/OEqZWJ",
           image: {
             imageURL:
-              'https://res.cloudinary.com/dv3lue3qa/image/upload/v1563170893/survey_form.png',
-            alt: 'Survey Form for Pizza delivery'
+              "https://res.cloudinary.com/dv3lue3qa/image/upload/v1563170893/survey_form.png",
+            alt: "Survey Form for Pizza delivery"
           }
         },
         {
-          name: 'Coffee Shop Page Layout',
-          url: 'https://codepen.io/iamlexieonearth/full/QxQbXX',
+          name: "Coffee Shop Page Layout",
+          url: "https://codepen.io/iamlexieonearth/full/QxQbXX",
           image: {
             imageURL:
-              'https://res.cloudinary.com/dv3lue3qa/image/upload/v1563171098/coffee-shop.png',
-            alt: 'Coffee Shop Page Layout with cosy vibe'
+              "https://res.cloudinary.com/dv3lue3qa/image/upload/v1563171098/coffee-shop.png",
+            alt: "Coffee Shop Page Layout with cosy vibe"
           }
         },
         {
-          name: 'Tribute Page',
-          url: 'https://codepen.io/iamlexieonearth/full/jGBRRE',
+          name: "Tribute Page",
+          url: "https://codepen.io/iamlexieonearth/full/jGBRRE",
           image: {
             imageURL:
-              'https://res.cloudinary.com/dv3lue3qa/image/upload/v1563171331/Tribute_Page_for_Free_Code_Camp_CSS_Grid_Layout.png',
-            alt: 'Tribute Page of Dr. B R  Ambedkar'
+              "https://res.cloudinary.com/dv3lue3qa/image/upload/v1563171331/Tribute_Page_for_Free_Code_Camp_CSS_Grid_Layout.png",
+            alt: "Tribute Page of Dr. B R  Ambedkar"
           }
         }
       ]
@@ -128,47 +125,47 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "../../src/assets/css/main.scss";
+
 .project__grid {
   position: relative;
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-  grid-auto-rows: auto;
+  grid-auto-rows: 40rem;
   grid-auto-flow: dense;
-  grid-gap: 5em;
+  grid-row-gap: 7em;
+  grid-column-gap: 5em;
   justify-self: center;
   align-self: center;
-  padding: 3em 3em 3em 3em;
 }
 
 img {
   width: 100%;
-  height: 250px;
+  height: 100%;
   object-fit: cover;
-  // border-radius: 2%;
   position: relative;
+  padding: 1em 0 1em 0;
 }
 
 .projectImage {
-  background:  rgb(196, 23, 23);
   cursor: pointer;
-  // border-radius: 2%;
   display: block;
   grid-auto-flow: column;
   top: 0;
   background-size: cover;
   background-position: 50% 50%;
+  transition: all 0.2s;
 }
 
 .projectImage:hover {
-  transform: scaleY(1.05);
-  transition: 10 ease-out;
-  box-shadow: 0 1px 2px rgb(196, 23, 23), 0 8px 0px rgb(196, 23, 23);
+  transform: translateY(-3px);
 }
 
 .project-title {
   text-align: center;
-  background:  rgb(196, 23, 23);
-  color: #fff;
+  color: rgb(196, 23, 23);
+  width: 100%;
+  height: 12%;
 }
 
 @media (max-width: 760px) {
